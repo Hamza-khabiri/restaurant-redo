cc490b5d91b9f72d91ce487f6e884c2d67448e41
hamza <hamzakhabiri@gmail.com>
2018-09-07 23:19:54 +0100

    finished all menus and added order toggle to menu icons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//smooth navigation
 $('a[href*= "#"]').not('[href="#"]').click(function (event) {
     if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
@@ -56,7 +57,6 @@ Date.prototype.addDays = function (days) {
 }
 
 //masonry style for menu elements
-//resizing grid-rows
 function getResizedItems(item) {
     menuList = document.querySelector(".restaurant-list");
     gridGap = parseInt(window.getComputedStyle(menuList).getPropertyValue('grid-row-gap'));
@@ -69,4 +69,42 @@ function resizeAllItems() {
     for (let i = 0; i < items.length; i++) {
         getResizedItems(items[i]);
     }
+}
+
+//menu switch buttons
+var resBtn = document.querySelector('#restaurantButton img');
+var cafBtn = document.querySelector('#cafeButton img');
+var pasBtn = document.querySelector('#pastryButton img');
+resBtn.type = 'restaurant';
+cafBtn.type = 'cafe';
+pasBtn.type = 'pastry';
+resBtn.addEventListener('click', openMenu, true);
+cafBtn.addEventListener('click', openMenu, true);
+pasBtn.addEventListener('click', openMenu, true);
+function openMenu(evt) {
+    var pastry = document.querySelector('.list-wrapper:nth-child(2)');
+    var cafe = document.querySelector('.list-wrapper:nth-child(3)');;
+    var restaurant = document.querySelector('.list-wrapper:nth-child(1)');
+    var type = evt.target.type;
+    switch (type) {
+        case 'restaurant':
+            restaurant.setAttribute('style', 'order: 1');
+            cafe.setAttribute('style', 'order: 2');
+            pastry.setAttribute('style', 'order: 3');
+            break;
+
+        case 'cafe':
+            restaurant.setAttribute('style', 'order: 3');
+            cafe.setAttribute('style', 'order: 1');
+            pastry.setAttribute('style', 'order: 2');
+
+            break;
+
+        case 'pastry':
+            restaurant.setAttribute('style', 'order: 2');
+            cafe.setAttribute('style', 'order: 3');
+            pastry.setAttribute('style', 'order: 1');
+            break;
+    }
+    resizeAllItems();
 }
